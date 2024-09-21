12fd925ea12429d0fa70ad5062f468d57898a53d
alfazsozib <alfazsozib@gmail.com>
2024-09-21 16:31:06 +0600

    done

---
+++ app.py
@@ -0,0 +1,105 @@
+import requests
+import time
+import pandas as pd
+import json
+import pymongo
+
+
+def scraper():
+    prices = ['0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09', '0.10', '0.11', '0.12', '0.13', '0.14', '0.15']
+    data_list = []
+    for price in prices:
+        url = f"https://www.viabtc.com/res/pool/miner?electricity_price={price}"
+        r = requests.get(url)
+        time.sleep(30)
+        response = r.json()
+        datas = response['data']['miners']
+        
+        for data in datas:
+            try:
+                name = data['name']
+            except:
+                name = ""
+            try:
+                coin = data['coin']
+            except:
+                coin = ''
+            try:
+                hashrate = data['hashrate']+" "+data["hash_unit"]
+            except:
+                hashrate = ""
+            try:
+                power = data['power']+" "+data['power_unit']
+            except:
+                power = ""
+            try:
+                unit_power = data["unit_power"]+" "+data['power_unit']+"/"+data["hash_unit"]
+            except:
+                unit_power = ""
+            try:
+                rev24h = data['unit_output_currency']
+            except:
+                rev24h = ""
+            try:
+                electricityFee = data['payout_currency']
+            except:
+                electricityFee = ""
+            try:
+                electricRatio = float(data['electricity_fee_percent'])*100
+            except:
+                electricRatio = ""
+            try:
+                netProfit24h = data['profit_currency']
+            except:
+                netProfit24h = ""
+            try:
+                breakEven = data['shutdown_coin_price']
+            except:
+                breakEven = ""
+
+                
+            temp = {
+                "Name":name,
+                "Coin":coin,
+                "Hash Rate":hashrate,
+                "Power":power,
+                "Unit Power":unit_power,
+                "Rev.24h":rev24h,
+                "Electricity Fee":electricityFee,
+                "Electricity Ratio":electricRatio,
+                "Net Profit 24h":netProfit24h,
+                "Break-Even":breakEven,
+                "User Electricity Fee": price
+            }
+            data_list.append(temp)
+            print(temp)
+            df = pd.DataFrame(data_list)
+            
+
+    df = pd.DataFrame(data_list)
+    df_sorted = df.sort_values(by=['Coin',"User Electricity Fee"])
+    # df_sorted.to_csv('miners_data.csv', index=False)
+
+    print("Data saved to miners_data.csv and sorted by coin.")
+    connectDatabase(df_sorted)
+
+def connectDatabase(df):
+    try:
+        client = pymongo.MongoClient(
+            "mongodb+srv://aspro1141:3j3nuNuRfKgwxslz@miners.cydxp.mongodb.net/?retryWrites=true&w=majority&appName=miners",
+            tls=True
+        )
+        db = client['minersInfo']
+        collection = db['miners']
+        
+        records = df.to_dict(orient='records')
+        if records:
+            collection.insert_many(records)
+            print("Inserted records into MongoDB")
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {e}")
+
+
+if __name__=="__main__":
+    scraper()
+
---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import pymongo
+from bson import ObjectId
+
+app = Flask(__name__)
+
+# MongoDB connection
+client = pymongo.MongoClient("mongodb+srv://aspro1141:3j3nuNuRfKgwxslz@miners.cydxp.mongodb.net/?retryWrites=true&w=majority&appName=miners")
+db = client['minersInfo']
+collection = db['miners']
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    coin = request.args.get('coin')
+    electricity_fee = request.args.get('electricity_fee')
+
+    if not coin or not electricity_fee:
+        return jsonify({"error": "Please provide both 'coin' and 'electricity_fee' parameters."}), 400
+
+    try:
+        results = collection.find({
+            "Coin": coin,
+            "User Electricity Fee": electricity_fee
+        })
+        
+        # Convert results to a list and handle ObjectId
+        data_list = []
+        for result in results:
+            result['_id'] = str(result['_id'])  # Convert ObjectId to string
+            data_list.append(result)
+        
+        return jsonify(data_list), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
